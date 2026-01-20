2d2ddb9c7a2943314ab1cb383303344224c6ab32
server 142 <server142@hotmail.com>
2026-01-20 00:07:26 -0600

    Emergency: Remove mandatory email verification and restore access

--- resources/views/emails/users/created.blade.php
+++ resources/views/emails/users/created.blade.php
@@ -8,10 +8,10 @@ Aquí tienes tus datos de acceso:
 **Usuario:** {{ $user->email }}
 **Contraseña:** {{ $password }}
 
-Puedes activar tu cuenta e iniciar sesión haciendo clic en el siguiente botón:
+Puedes iniciar sesión haciendo clic en el siguiente botón:
 
-<x-mail::button :url="$verificationUrl">
-Activar Cuenta e Iniciar Sesión
+<x-mail::button :url="route('login')">
+Iniciar Sesión
 </x-mail::button>
 
 Por seguridad, te recomendamos cambiar tu contraseña una vez que hayas ingresado.
