e74ccba58ac8407f1dc9557368a1dff6238e0dff
server 142 <server142@hotmail.com>
2026-02-02 19:24:31 -0600

    Fix: Corrección de variable indefinida  en la vista de gestión de planes

--- resources/views/livewire/admin/plans/manage.blade.php
+++ resources/views/livewire/admin/plans/manage.blade.php
@@ -5,7 +5,7 @@
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
             </a>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ $plan_id ? __('Editar Plan') : __('Nuevo Plan') }}
+                {{ $plan?->id ? __('Editar Plan') : __('Nuevo Plan') }}
             </h2>
         </div>
     </x-slot>
