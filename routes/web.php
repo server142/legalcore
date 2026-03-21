f02f27c5dc5c8e60d05f5e360e0d1fd37aa54eba
server 142 <server142@hotmail.com>
2026-03-21 10:39:45 -0600

    exclude directory plans from main landing (diogenes.com.mx)

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
     $plans = Plan::where('is_active', true)
         ->where('slug', '!=', 'trial')
         ->where('slug', '!=', 'exento')
+        ->where('slug', 'not like', '%directory%')
         ->orderBy('price', 'asc')
         ->get();
     
